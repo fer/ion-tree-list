1a3717e9272b4ad01b25838abe818e887a18601d
fer <fer@ferqwerty.com>
2015-05-20 13:35:25 +0200

    wip styling items

--- README.md
+++ README.md
@@ -3,3 +3,48 @@
 Ionic directive for displaying and manipulating nested list ionic items.
 
 <!-- Here is the DEMO page -->
+
+## Installation
+
+<!-- bower installation -->
+
+
+```
+<script src="lib/ion-tree-list/ion-tree-list.js"></script>
+```
+
+app.js: 
+
+```
+angular.module('starter', [
+    'ionic', 
+    'controllers', 
+    'services', 
+    'ion-tree-list'
+])
+```
+
+controller:
+
+```
+  $scope.tasks = [
+    {
+      name: 'first task 1',
+      tree: [
+        {
+          name: 'first task 1.1'
+        },
+      ]
+    },
+    {
+      name: 'first task 2'
+    }
+  ];    
+```
+
+
+Template:
+
+```
+<ion-tree-list items="tasks"></ion-tree-list>
+```
--- ion-tree-list.js
+++ ion-tree-list.js
@@ -1,28 +1,37 @@
+/**
+ * returns a tree object with extra attribute 'depth'
+ *
+ * @param obj tree object
+ * @param depth initial depth
+ * @returns {*} obj with depths
+ */
 function addDepthToTree(obj, depth) {
     for (key in obj) {
         if (typeof(obj[key]) == 'object') {
-            if (key == 'tree') {
-                addDepthToTree(obj[key], depth)
-            }
-
             obj[key].depth = depth;
-            addDepthToTree(obj[key], depth + 1)
+            obj[key].visible = true;
+
+            addDepthToTree(obj[key], (key == 'tree') ? depth + 1 : depth)
         }
     }
     return obj
 }
 
 angular.module('ion-tree-list', [])
-    .directive('ionTreeList', function () {
-        return {
-            restrict: 'EA',
-            scope: {
-                items: '='
-            },
-            templateUrl: 'lib/ion-tree-list/ion-tree-list.tmpl.html',
-            controller: function($scope){
-                $scope.items = addDepthToTree($scope.items, 0);
-                console.log($scope.items)
+.directive('ionTreeList', function () {
+    return {
+        restrict: 'E',
+        transclude: true,
+        scope: {
+            items: '='
+        },
+        templateUrl: 'lib/ion-tree-list/ion-tree-list.tmpl.html',
+        controller: function($scope){
+            $scope.items = addDepthToTree($scope.items, 0);
+            $scope.collapse = function(item) {
+                item.visible = false;
+                console.log(item);
             }
         }
-    });
+    }
+});

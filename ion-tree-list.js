e66e9cc0c6d7488fcdcc0a40a568da8d5ed99b0f
fer <fer@ferqwerty.com>
2015-05-20 15:18:17 +0200

    wip collapse method (fix toggled items)

--- ion-tree-list.js
+++ ion-tree-list.js
@@ -9,11 +9,26 @@ function addDepthToTree(obj, depth) {
     for (key in obj) {
         if (typeof(obj[key]) == 'object') {
             obj[key].depth = depth;
-            obj[key].visible = true;
-
-            addDepthToTree(obj[key], (key == 'tree') ? depth + 1 : depth)
+            addDepthToTree(obj[key], key === 'tree' ? depth + 1 : depth)
         }
     }
+    return obj
+}
+
+/**
+ * adds visible attribute and toggles its value in case it exists
+ *
+ * @param obj
+ * @returns {*}
+ */
+function collapse(obj) {
+    for (key in obj) {
+        if (typeof(obj[key]) == 'object') {
+            obj[key].collapsed = !obj[key].collapsed;
+            collapse(obj[key])
+        }
+    }
+
     return obj
 }
 
@@ -27,11 +42,9 @@ angular.module('ion-tree-list', [])
         },
         templateUrl: 'lib/ion-tree-list/ion-tree-list.tmpl.html',
         controller: function($scope){
+            // Add depth attr to items
             $scope.items = addDepthToTree($scope.items, 0);
-            $scope.collapse = function(item) {
-                item.visible = false;
-                console.log(item);
-            }
+            $scope.collapse = collapse
         }
     }
 });

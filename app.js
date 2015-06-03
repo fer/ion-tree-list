5d8aef051feaa0b42cfd2469c6eeb028f67932b3
fer <fer@ferqwerty.com>
2015-06-03 02:27:15 +0200

    closes #12 added custom template demo

--- app.js
+++ app.js
@@ -1,13 +1,14 @@
 angular.module('starter', ['ionic', 'ion-tree-list'])
     .controller('DashCtrl', function($scope) {
         $scope.collapse = true;
-
         $scope.tasks = [
             {
                 name: 'Chapter one',
+                checked: false,
                 tree: [
                     {
                         name: 'Section 1',
+                        checked: false,
                         tree: [
                             {
                                 name: 'Subsection'
@@ -15,26 +16,42 @@ angular.module('starter', ['ionic', 'ion-tree-list'])
                         ]
                     },
                     {
-                        name: 'Section 2'
+                        name: 'Section 2',
+                        checked: true
                     },
                     {
-                        name: 'Section 3'
+                        name: 'Section 3',
+                        checked: true
                     }
                 ]
             },
             {
-                name: 'Chapter two'
+                name: 'Chapter two',
+                checked: true
             },
             {
-                name: 'Chapter three'
+                name: 'Chapter three',
+                checked: false
+
             },
             {
-                name: 'Chapter four'
+                name: 'Chapter four',
+                checked: true
             }
         ];
 
         $scope.toggleCollapse = function(){
             $scope.collapse = !$scope.collapse;
-            console.log($scope.collapse);
+            console.log($scope.collapse)
+        };
+
+        $scope.customTemplate = 'item_default_renderer';
+
+        $scope.toggleTemplate = function() {
+            if ($scope.customTemplate == 'ion-item.tmpl.html') {
+                $scope.customTemplate = 'item_default_renderer'
+            } else {
+                $scope.customTemplate = 'ion-item.tmpl.html'
+            }
         }
     });

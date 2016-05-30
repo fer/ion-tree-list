4e40d996bd4de656cab7709cee09ffcd99276d87
fer <fer@users.noreply.github.com>
2016-05-30 10:56:30 +0200

    Update ion-tree-list.js

--- ion-tree-list.js
+++ ion-tree-list.js
@@ -4,11 +4,11 @@
 var CONF = {
     baseUrl: 'lib/ion-tree-list',
     digestTtl: 35
-}
+};
 
 function addDepthToTree(obj, depth, collapsed) {
     for (var key in obj) {
-        if ( obj[key] && typeof(obj[key]) == 'object') {
+        if (obj[key] && typeof(obj[key]) == 'object') {
             obj[key].depth = depth;
             obj[key].collapsed = collapsed;
             addDepthToTree(obj[key], key === 'tree' ? ++ depth : depth, collapsed)
